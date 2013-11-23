7e9b1aaafaa15ed620bf8e3eeeabb7a3b36827d0
Vlad Lastname <warangel.alk@gmail.com>
2013-11-23 09:34:30 +0200

    Created first files

---
+++ src/login/LoginFrame.java
@@ -0,0 +1,20 @@
+package login;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Dimension;
+
+public class LoginFrame extends JFrame {
+
+	public LoginFrame() {
+		
+	}
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import login.LoginFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new LoginFrame();
+	}
+
+}
